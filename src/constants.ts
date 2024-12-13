19809c88e459e49e50def4f6f2241c0a2144721c
Ivan Shoshkov <ishoshkov@tpviewnyc.com>
2024-12-13 17:03:57 +0200

    clean up db and ignore dotenv

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,4 @@
-export const BASE_URL =
-  process.env.REACT_APP_BASE_URL || "http://localhost:3000";
+export const BASE_URL = "http://localhost:8000";
 
 export const CREATE = "CREATE";
 export const UPDATE = "UPDATE";
